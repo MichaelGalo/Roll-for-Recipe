bea7a703d64a9e2b56322036bdf56822386aa7cc
Michael Galo <michael@michaelthinks.co>
2024-07-18 11:05:02 -0500

    added basic directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+// this module is where I need to set up my routes for login, register, and *
+
 import "./App.css";
 
-function App() {
+export const App = () => {
   return (
     <>
       <h1>Roll for Recipe</h1>
       <h2>Coming Soon</h2>
     </>
   );
-}
-
-export default App;
+};
---
+++ src/views/Authorized.jsx
@@ -0,0 +1,1 @@
+// this module is one that I'll like need to more or less copy and paste from learning-moments / update with the one steve did (death and money)
