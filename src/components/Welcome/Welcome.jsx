3dbe82df884ce39adb6f8e970b3037ce509a8585
Michael Galo <michael@michaelthinks.co>
2024-07-18 13:31:01 -0500

    added basic welcome splash page

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,13 @@
+import "./Welcome.css";
+
+export const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1>Welcome to Recipe Roll!</h1>
+      <h2>
+        Where you can roll for a recipe, save your favorites, and create your
+        own recipes!
+      </h2>
+    </div>
+  );
+};
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,12 +1,59 @@
 // this is the primary module where I will route my application
 
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Welcome } from "../components/Welcome/Welcome";
+
 export const ApplicationViews = () => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const localRecipeUser = localStorage.getItem("recipe_token");
+    setCurrentUser(JSON.parse(localRecipeUser));
+  }, []);
+
   return (
-    <>
-      <h1>Roll for Recipe</h1>
-      <h2>Coming Soon</h2>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            {<div>A Navbar with passed currentUser will be here</div>}
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<Welcome />} />
+        <Route path="all-recipes" element={<div>All Recipes</div>} />
+        <Route
+          path="recipe-details/:recipeId"
+          element={<div>Recipe Details</div>}
+        />
+        <Route path="new-recipe" element={<div>New Recipe</div>} />
+        <Route path="my-recipes" element={<div>My Recipes</div>} />
+        <Route path="edit-recipe/:recipeId" element={<div>Edit Recipe</div>} />
+        <Route path="favorites" element={<div>Favorites</div>} />
+        <Route path="profile/:userId" element={<div>Profile</div>} />
+        <Route path="edit-profile/:userId" element={<div>Edit Profile</div>} />
+        <Route path="roll-for-recipe" element={<div>Roll for Recipe</div>} />
+        <Route path="shopping-list" element={<div>Shopping List</div>} />
+      </Route>
+    </Routes>
   );
 };
 
-//TODO: Begin working on the route structure for the application
+// Here are the Routes I Need
+
+// All Recipes
+// Recipe Details
+// New Recipe
+// My Recipes
+// Edit Recipe
+// Favorites
+// Profile
+// Edit Profile
+// Roll for Recipe
+// Shopping List
+
+// TODO: NavBar
