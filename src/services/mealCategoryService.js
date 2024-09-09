0376e1b19130bf2cb5dd89ba6184e5181b60d1a8
Michael Galo <michael@michaelthinks.co>
2024-09-09 15:37:48 -0500

    Refactored mealCategoryService for firebase

--- src/services/mealCategoryService.js
+++ src/services/mealCategoryService.js
@@ -1,5 +1,24 @@
+// export const getMealCategories = async () => {
+//   return await fetch(`http://localhost:8088/mealTypes`).then((res) =>
+//     res.json()
+//   );
+// };
+
+////////////////////////////////////////////
+// Firebase version of the above functions
+////////////////////////////////////////////
+
+import { ref, get } from "firebase/database";
+import { database } from "./firebase"; // import the initialized Firebase database
+
+// Get Meal Categories
 export const getMealCategories = async () => {
-  return await fetch(`http://localhost:8088/mealTypes`).then((res) =>
-    res.json()
-  );
+  const mealCategoriesRef = ref(database, "mealTypes"); // reference to 'mealTypes' collection
+  const snapshot = await get(mealCategoriesRef);
+
+  if (snapshot.exists()) {
+    return snapshot.val(); // return the meal categories
+  } else {
+    return null; // no meal categories found
+  }
 };
