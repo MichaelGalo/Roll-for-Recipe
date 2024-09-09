2e9fc01e7d696ea881cc20ad4a5d8db54b3e1e3a
Michael Galo <michael@michaelthinks.co>
2024-09-09 08:53:21 -0500

    Added initialization docs and tools for firebase

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWyAQ5XTPHa76bfxSMO_DGRfW6oKs_w2A",
+  authDomain: "roll-for-recipe.firebaseapp.com",
+  databaseURL: "https://roll-for-recipe-default-rtdb.firebaseio.com",
+  projectId: "roll-for-recipe",
+  storageBucket: "roll-for-recipe.appspot.com",
+  messagingSenderId: "1079068155032",
+  appId: "1:1079068155032:web:7f87692850394edc7744ed",
+  measurementId: "G-Q0SEV92DRB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
