1a9e93f4df869c887778c0bd10dde2d0410ab34c
Michael Galo <michael@michaelthinks.co>
2024-09-11 06:53:34 -0500

    Attempting to add security practices for sensitive db info

--- firebase.js
+++ firebase.js
@@ -6,14 +6,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWyAQ5XTPHa76bfxSMO_DGRfW6oKs_w2A",
-  authDomain: "roll-for-recipe.firebaseapp.com",
-  databaseURL: "https://roll-for-recipe-default-rtdb.firebaseio.com",
-  projectId: "roll-for-recipe",
-  storageBucket: "roll-for-recipe.appspot.com",
-  messagingSenderId: "1079068155032",
-  appId: "1:1079068155032:web:7f87692850394edc7744ed",
-  measurementId: "G-Q0SEV92DRB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
